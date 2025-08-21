b33d17539ccff18eaca5e788ffedda644d4e2b03
Hael <michelkleinth@gmail.com>
2025-08-20 21:00:42 -0300

    initial commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,28 @@
+// setup atributo especial feito para manipular o escript
+<script setup>
+const nome = "Michel klein"
+const meuObj = {
+  nome: "Michel",
+  filmeFavorito: "Enigma de outro mundo"
+}
+
+function dizOla(nome) {
+  return `${nome} diz oi`;
+}
+
+const endereçoDaImagem = "https://cdn.pixabay.com/photo/2025/08/11/10/07/milky-way-9767930_1280.jpg"
+
+const botaoEstaDesabilitado = false
+</script>
+
+<template>
+  <h1>{{ dizOla("Paula") }}</h1>
+  <img :src="endereçoDaImagem">
+
+  <button :disable="botaoEstaDesabilitado">Enviar mensagem</button>
+</template>
+
+
+// scoped mostra que sera apenas utilizado aqui, não vaza para outros arquivos
+<style scoped>
+</style>
