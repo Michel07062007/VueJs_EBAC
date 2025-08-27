85ce43c02191cd852b482bce02cac4fff1c29589
Hael <michelkleinth@gmail.com>
2025-08-27 15:02:02 -0300

    fazendo a renderização condicional

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -11,13 +11,26 @@ function dizOla(nome) {
 }
 
 const endereçoDaImagem = "https://cdn.pixabay.com/photo/2025/08/11/10/07/milky-way-9767930_1280.jpg"
+const imagenAleatoria = "https://cdn.pixabay.com/photo/2025/08/11/10/07/milky-way-9767930_1280.jpg"
 
 const botaoEstaDesabilitado = false
+
+const gostaDoBatman = true
+const gostaDoSuperMan = true
+
+const estaAutorizado = false
 </script>
 
 <template>
   <h1>{{ dizOla("Paula") }}</h1>
-  <img :src="endereçoDaImagem">
+  <img v-if="gostaDoBatman" :src="endereçoDaImagem">
+  <img v-else-if="gostaDoSuperMan" :src="imagenAleatoria">
+  <h2 v-else>Não curte heróis da DC</h2>
+
+  <h1 v-if="estaAutorizado">Bem-vindo</h1>
+  <h2 v-else>Não possui acesso</h2>
+
+  <h1></h1>
 
   <button :disable="botaoEstaDesabilitado">Enviar mensagem</button>
 </template>
@@ -25,4 +38,9 @@ const botaoEstaDesabilitado = false
 
 // scoped mostra que sera apenas utilizado aqui, não vaza para outros arquivos
 <style scoped>
+
+img { 
+  max-width: 200px;
+}
+
 </style>
